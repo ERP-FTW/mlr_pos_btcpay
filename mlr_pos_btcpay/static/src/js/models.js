e3138ee19500c82d4c66dfed684161368e93a707
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2024-01-20 20:36:13 -0500

    Delete mlr_pos_btcpay directory

--- mlr_pos_btcpay/static/src/js/models.js
+++
@@ -1,6 +0,0 @@
-odoo.define('pos_btcpaypayment.models', function (require) {
-var models = require('point_of_sale.models');
-var PaymentbtcpayPayment = require('pos_btcpaypayment.payment');
-
-models.register_payment_method('btcpay', PaymentbtcpayPayment);
-});
